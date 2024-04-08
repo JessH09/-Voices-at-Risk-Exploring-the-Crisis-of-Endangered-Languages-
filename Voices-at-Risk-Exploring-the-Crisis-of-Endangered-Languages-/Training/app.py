7512905920313ebc533d46e391dd921c309871ee
JessH09 <jesshernan0919@gmail.com>
2024-04-08 18:55:06 -0400

    adding in code that takes user from one page to another adding in bar html

--- Voices-at-Risk-Exploring-the-Crisis-of-Endangered-Languages-/Training/app.py
+++ Voices-at-Risk-Exploring-the-Crisis-of-Endangered-Languages-/Training/app.py
@@ -1,51 +1,45 @@
-from flask import Flask, render_template,send_from_directory
+from flask import Flask, render_template, redirect, url_for
  
 # @app.route('/')
 # def index():
 #     return '<img src="/static/images/bar.png" alt="Number of Languages">'
-
-
-
 app = Flask(__name__)
 
-# home page route
 @app.route('/')
 def index():
     return render_template('index.html')
 
-#map page route  
-# @app.route('/map')  
-# def map():
-#     # Code to fetch data and pass to template
-#     # return render_template('index.html', data=data)
-#     return '<img src="/resources/map" alt="Number of Languages">'
-
-# bar chart page route
+# Bar chart page route
 @app.route('/bar_chart')
 def bar_chart():
     # Code to generate bar chart using pandas and Matplotlib
     # Save the bar chart as an image file
     # Pass the image file path to the template
-    return '<img src="/static/images/bar.png" alt="Number of Languages">'
-    return redirect(url_for('index'))
-                           
-
-# if __name__ == '__main__':
+    # For example, assuming the image is saved as 'bar.png' in the 'static/images' folder
+    image_path = '/static/images/bar.png'
+    text = "This bar chart shows the total number of languages per endangerment level"
+    return render_template('bar_chart.html', image_path=image_path, text=text)
 
 @app.route('/pie_chart')
 def pie_chart():
     # Code to generate line chart using pandas and Matplotlib
     # Save the line chart as an image file
     # Pass the image file path to the template
-    return '<img src="/static/images/pie_chart.png" alt="percentage of languages per endangerment">'
-    return redirect(url_for('index'))
+    image_path = '/static/images/pie_chart.png'
+    text = "This pie chart shows the percentage of languages that are at each level"
+
+    return render_template('bar_chart.html', image_path=image_path, text=text)
+
+    # return redirect(url_for('index'))
 
 @app.route('/avg_speakers')
 def donut_chart():
     # Code to generate line chart using pandas and Matplotlib
     # Save the line chart as an image file
     # Pass the image file path to the template
-    return '<img src="/static/images/avg_speakers.png" alt="avergage number of speakers left">'
-    return redirect(url_for('index'))
+    image_path = '/static/images/avg_speakers.png'
+    text = "This chart shows the average number of speakers per endangerment level"
+    return render_template('bar_chart.html', image_path=image_path, text=text)
 
+    # return redirect(url_for('index'))
 app.run(debug=True)
